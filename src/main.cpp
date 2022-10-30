de431ebe1f91cd178ff94e4f3e9d89145c7285d8
Parker Barrett <parkerb7@vt.edu>
2022-10-30 17:44:07 -0400

    Skeleton structure for GPS-INS repository

---
+++ src/main.cpp
@@ -0,0 +1,40 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                GPS-Aided Inertial Navigation System                              //
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                  // 
+// Author:      Parker Barrett                                                                      //
+// Overview:    The main GPS-Aided Inertial Navigation system executable. This top level            //
+//              executable creates the navigation thread which runs the strapdown integrator, INS   //
+//              measurement compensator and error state extended Kalman Filter. It also sets up     //
+//              the ROS publishers and subscribers.                                                 //                                                                         //           
+//                                                                                                  //
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Include Headers
+#include <iostream>
+#include <thread>
+
+// Main GPS-INS Function
+int main(int argc, char **argv) {
+
+    // Unpack Inputs
+    
+    // Create ROS Input Subscribers
+
+    // Create ROS Output Publishers
+
+    // Measurement Compensator Thread
+
+    // Strapdown Integration Thread
+
+    // Kalman Filter Thread
+
+    // Terminate Program
+
+    // Temp
+    std::cout << "Temporary Hold - GPS-INS Main Function" << std::endl;
+    
+    // Successful Return
+    return 0;
+
+}
